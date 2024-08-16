53472533db2d85d9a34389a54e0efdc9afa4795b
xtrinch <mojca.rojko@povio.com>
2024-08-16 16:05:41 +0200

    More cleanup

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -4,9 +4,9 @@ use cell_draw::CellDrawContext;
 use cgmath::Vector2;
 use input::Input;
 use sdl2::timer::Timer;
-use sdl2::ttf::Font;
-use sdl2::EventSubsystem;
+use sdl2::ttf::{Font, Sdl2TtfContext};
 use sdl2::{event::Event, pixels::Color, rect::Rect, render::Canvas, video::Window};
+use sdl2::{EventPump, EventSubsystem, Sdl, VideoSubsystem};
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::path::Path;
@@ -22,10 +22,6 @@ mod render_traits;
 mod sub_rect;
 mod text_draw;
 
-pub struct Interface {
-    pub engine: Engine,
-}
-
 const INIT_SIZE: Vector2<u32> = Vector2::new(1024, 1024);
 const BACKGROUND_COLOR: Color = Color::RGB(0x10, 0x10, 0x18);
 const MATRIX_COLOR: Color = Color::RGB(0x66, 0x77, 0x77);
@@ -38,6 +34,14 @@ struct Tick; // basically same as type Tick=()
 struct LockdownTick;
 struct SoftDropTick;
 struct Sleep(Duration);
+
+pub struct Interface {
+    pub engine: Engine,
+    pub sdl: Sdl,
+    pub canvas: Canvas<Window>,
+    pub events: EventPump,
+    pub ttf_context: Sdl2TtfContext,
+}
 
 impl Interface {
     // fn flush_and_readd_events() {
@@ -54,16 +58,10 @@ impl Interface {
     // }
 
     pub fn new(mut engine: Engine) -> Self {
-        Self { engine }
-    }
-
-    pub fn run(&mut self) -> Result<(), String> {
-        let sdl = sdl2::init().expect("Failed to initialize sdl2");
-
+        let sdl: Sdl = sdl2::init().expect("Failed to initialize sdl2");
+        let video = sdl.video().expect("Failed to acquire display");
         let mut canvas = {
             // evaluation block
-            let video = sdl.video().expect("Failed to acquire display");
-
             let window = video
                 .window("Tetris", INIT_SIZE.x, INIT_SIZE.y)
                 .position_centered()
@@ -78,20 +76,25 @@ impl Interface {
                 .build()
                 .expect("Failed to get render canvas")
         };
+        let mut events = sdl.event_pump().expect("Failed to get event pump");
 
         let ttf_context = sdl2::ttf::init()
             .map_err(|e| e.to_string())
             .expect("Failed to initialize ttf context");
 
-        // Load the font
-        let path: &Path = Path::new("assets/NewAmsterdam-Regular.ttf");
-        let mut font = ttf_context
-            .load_font(path, 512)
-            .expect("Failed to load font");
+        Self {
+            engine,
+            sdl,
+            canvas,
+            events,
+            ttf_context,
+        }
+    }
 
-        let mut events = sdl.event_pump().expect("Failed to get event pump");
-
-        // event_subsystem.push_custom_event(Tick).unwrap();
+    pub fn run(&mut self) -> Result<(), String> {
+        let static_event_subsystem: &'static _ = Box::leak(Box::new(
+            self.sdl.event().expect("Failed to acquire event subsystem"),
+        ));
 
         /*
         A tetrimino that is Hard dropped Locks down immediately. However, if a tetrimino
@@ -109,9 +112,6 @@ impl Interface {
         let mut is_soft_drop = false;
         let mut locking_down = false; // TODO: perhaps best to have a "state" enum instead of relying on this
 
-        let static_event_subsystem: &'static _ = Box::leak(Box::new(
-            sdl.event().expect("Failed to acquire event subsystem"),
-        ));
         static_event_subsystem
             .register_custom_event::<Tick>()
             .unwrap();
@@ -124,7 +124,7 @@ impl Interface {
         static_event_subsystem.push_custom_event(Tick).unwrap();
 
         loop {
-            for event in events.poll_iter() {
+            for event in self.events.poll_iter() {
                 // match dbg!(event) {
                 match event {
                     // log any events with dbg
@@ -301,205 +301,214 @@ impl Interface {
                 locking_down = false;
             }
             if dirty {
-                draw(&mut canvas, &mut font, &self.engine);
+                self.draw();
             }
             dirty = false;
         }
     }
-}
 
-fn draw(canvas: &mut Canvas<Window>, font: &mut Font, engine: &Engine) {
-    canvas.set_draw_color(BACKGROUND_COLOR);
-    canvas.clear();
-    canvas.set_draw_color(Color::WHITE);
+    fn draw(&mut self) {
+        // Load the font
+        let path: &Path = Path::new("assets/NewAmsterdam-Regular.ttf");
+        let mut font = self
+            .ttf_context
+            .load_font(path, 512)
+            .expect("Failed to load font");
 
-    let viewport = canvas.viewport();
+        self.canvas.set_draw_color(BACKGROUND_COLOR);
+        self.canvas.clear();
+        self.canvas.set_draw_color(Color::WHITE);
 
-    // the design is all based upon a 16x15 grid which is further divided into 4ths (see grid.png) -
-    // the system is based upon first positioning the container, then an inner rect relative to id
+        let viewport = self.canvas.viewport();
 
-    // the square into which we draw and the margin which can be either on the left/right or top/bottom (because the window is resizable)
-    // let ui_square = {
-    //     let Vector2 { x, y } = Vector2::from(viewport.size()).cast::<i32>().unwrap();
+        // the design is all based upon a 16x15 grid which is further divided into 4ths (see grid.png) -
+        // the system is based upon first positioning the container, then an inner rect relative to id
 
-    //     if x > y {
-    //         // landscape, we have top and bottom black margins
-    //         let midpoint = x / 2;
-    //         let left_edge = midpoint - (y / 2);
-    //         Rect::new(left_edge, 0, y as u32, y as u32)
-    //     } else {
-    //         // portrait, we have left and right black margins
-    //         let midpoint = y / 2;
-    //         let top_edge = midpoint - (x / 2);
-    //         Rect::new(0, top_edge, x as u32, x as u32)
-    //     }
-    // };
-    // canvas.draw_rect(ui_square).unwrap();
+        // the square into which we draw and the margin which can be either on the left/right or top/bottom (because the window is resizable)
+        // let ui_square = {
+        //     let Vector2 { x, y } = Vector2::from(viewport.size()).cast::<i32>().unwrap();
 
-    // the square into which we draw and the margin which can be either on the left/right or top/bottom (because the window is resizable)
-    let ui_square1 = SubRect::absolute(viewport, (1.0, 1.0), None);
+        //     if x > y {
+        //         // landscape, we have top and bottom black margins
+        //         let midpoint = x / 2;
+        //         let left_edge = midpoint - (y / 2);
+        //         Rect::new(left_edge, 0, y as u32, y as u32)
+        //     } else {
+        //         // portrait, we have left and right black margins
+        //         let midpoint = y / 2;
+        //         let top_edge = midpoint - (x / 2);
+        //         Rect::new(0, top_edge, x as u32, x as u32)
+        //     }
+        // };
+        // canvas.draw_rect(ui_square).unwrap();
 
-    let matrix_container = ui_square1.sub_rect((0.5, 1.0), None); // half of the width and full height, center alignment by default
+        // the square into which we draw and the margin which can be either on the left/right or top/bottom (because the window is resizable)
+        let ui_square1 = SubRect::absolute(viewport, (1.0, 1.0), None);
 
-    let matrix1 = ui_square1
-        .sub_rect((0.5, 1.0), None) // half of the width and full height, center alignment by default
-        .sub_rect((7.0 / 8.0, 7.0 / 8.0), None); // 7/8ths of the width and 7/8ths of the height, center by default
+        let matrix_container = ui_square1.sub_rect((0.5, 1.0), None); // half of the width and full height, center alignment by default
 
-    // top right container for coming up tetrimino
-    let up_next1 = ui_square1
-        .sub_rect((0.25, 0.25), Some((Align::Far, Align::Near))) // top right container
-        .sub_rect((7.0 / 8.0, 7.0 / 8.0), Some((Align::Center, Align::Center))); // inside the top right container
+        let matrix1 = ui_square1
+            .sub_rect((0.5, 1.0), None) // half of the width and full height, center alignment by default
+            .sub_rect((7.0 / 8.0, 7.0 / 8.0), None); // 7/8ths of the width and 7/8ths of the height, center by default
 
-    // top left container for hold tetrimino
-    let hold1 = ui_square1
-        .sub_rect((0.25, 0.25), Some((Align::Near, Align::Near)))
-        .sub_rect((0.64, 0.64), None);
+        // top right container for coming up tetrimino
+        let up_next1 = ui_square1
+            .sub_rect((0.25, 0.25), Some((Align::Far, Align::Near))) // top right container
+            .sub_rect((7.0 / 8.0, 7.0 / 8.0), Some((Align::Center, Align::Center))); // inside the top right container
 
-    // bottom right where next tetriminos are displayed
-    let queue1 = ui_square1
-        .sub_rect((0.25, 0.75), Some((Align::Far, Align::Far)))
-        .sub_rect(
-            (5.0 / 10.0, 23.0 / 24.0),
-            Some((Align::Center, Align::Near)),
-        );
+        // top left container for hold tetrimino
+        let hold1 = ui_square1
+            .sub_rect((0.25, 0.25), Some((Align::Near, Align::Near)))
+            .sub_rect((0.64, 0.64), None);
 
-    // bottom left score box
-    let score1 = ui_square1
-        .sub_rect((0.25, 11.0 / 16.0), Some((Align::Near, Align::Far)))
-        .sub_rect((7.0 / 8.0, 8.0 / 11.0), Some((Align::Center, Align::Near)));
+        // bottom right where next tetriminos are displayed
+        let queue1 = ui_square1
+            .sub_rect((0.25, 0.75), Some((Align::Far, Align::Far)))
+            .sub_rect(
+                (5.0 / 10.0, 23.0 / 24.0),
+                Some((Align::Center, Align::Near)),
+            );
 
-    canvas.set_draw_color(MATRIX_CONTAINER_COLOR);
-    canvas.fill_rect(Rect::from(matrix_container)).unwrap();
+        // bottom left score box
+        let score1 = ui_square1
+            .sub_rect((0.25, 11.0 / 16.0), Some((Align::Near, Align::Far)))
+            .sub_rect((7.0 / 8.0, 8.0 / 11.0), Some((Align::Center, Align::Near)));
 
-    canvas.set_draw_color(MATRIX_COLOR);
+        self.canvas.set_draw_color(MATRIX_CONTAINER_COLOR);
+        self.canvas.fill_rect(Rect::from(matrix_container)).unwrap();
 
-    for subrect in [&matrix1, &up_next1, &hold1, &queue1, &score1] {
-        canvas.fill_rect(Rect::from(subrect)).unwrap();
-    }
+        self.canvas.set_draw_color(MATRIX_COLOR);
 
-    let mut cell_draw_ctx: CellDrawContext<{ Engine::MATRIX_WIDTH }, { Engine::MATRIX_HEIGHT }> =
-        CellDrawContext {
+        for subrect in [&matrix1, &up_next1, &hold1, &queue1, &score1] {
+            self.canvas.fill_rect(Rect::from(subrect)).unwrap();
+        }
+
+        let mut cell_draw_ctx: CellDrawContext<
+            { Engine::MATRIX_WIDTH },
+            { Engine::MATRIX_HEIGHT },
+        > = CellDrawContext {
             origin: matrix1.bottom_left(),
             dims: Vector2::from(matrix1.size()),
-            canvas,
-            matrix: &engine.matrix, // TODO: figure our how to pass the iter instead of the whole matrix
+            canvas: &mut self.canvas,
+            matrix: &self.engine.matrix, // TODO: figure our how to pass the iter instead of the whole matrix
         };
 
-    cell_draw_ctx.draw_matrix();
+        cell_draw_ctx.draw_matrix();
 
-    if let Some((cursor_cells, cursor_color, _)) = engine.cursor_info() {
-        for coord in cursor_cells {
-            cell_draw_ctx.try_draw_cell(coord, Some(cursor_color));
+        if let Some((cursor_cells, cursor_color, _)) = self.engine.cursor_info() {
+            for coord in cursor_cells {
+                cell_draw_ctx.try_draw_cell(coord, Some(cursor_color));
+            }
         }
+
+        let mut up_next_cell_draw_ctx: CellDrawContext<
+            { Engine::SINGLE_TETRIMINO_MATRIX_WIDTH },
+            { Engine::SINGLE_TETRIMINO_MATRIX_HEIGHT },
+        > = CellDrawContext {
+            origin: up_next1.bottom_left(),
+            dims: Vector2::from(up_next1.size()),
+            canvas: &mut self.canvas,
+            matrix: &self.engine.up_next_matrix,
+        };
+
+        up_next_cell_draw_ctx.draw_matrix();
+
+        let mut remaining_next_cell_draw_ctx: CellDrawContext<
+            { Engine::REMAINING_NEXT_MATRIX_WIDTH },
+            { Engine::REMAINING_NEXT_MATRIX_HEIGHT },
+        > = CellDrawContext {
+            origin: queue1.bottom_left(),
+            dims: Vector2::from(queue1.size()),
+            canvas: &mut self.canvas,
+            matrix: &self.engine.queue_matrix,
+        };
+
+        remaining_next_cell_draw_ctx.draw_matrix();
+
+        let mut hold_cell_draw_ctx: CellDrawContext<
+            { Engine::SINGLE_TETRIMINO_MATRIX_WIDTH },
+            { Engine::SINGLE_TETRIMINO_MATRIX_HEIGHT },
+        > = CellDrawContext {
+            origin: hold1.bottom_left(),
+            dims: Vector2::from(hold1.size()),
+            canvas: &mut self.canvas,
+            matrix: &self.engine.hold_matrix,
+        };
+
+        hold_cell_draw_ctx.draw_matrix();
+
+        // up next text
+        let up_next_text = up_next1.sub_rect((0.5, 0.2), Some((Align::Center, Align::Near)));
+
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: "UP NEXT",
+            rect: up_next_text,
+        };
+        text_draw_ctx.draw_text();
+
+        // hold text
+        let hold_text = hold1.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
+
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: "HOLD",
+            rect: hold_text,
+        };
+        text_draw_ctx.draw_text();
+
+        let score_top = score1.sub_rect((1.0, 0.5), Some((Align::Center, Align::Near)));
+        let score_bottom = score1.sub_rect((1.0, 0.5), Some((Align::Center, Align::Far)));
+
+        // level text
+        let level_text = score_top.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
+
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: "LEVEL",
+            rect: level_text,
+        };
+        text_draw_ctx.draw_text();
+
+        // level text
+        let level_text = score_top.sub_rect((0.8, 0.85), Some((Align::Center, Align::Far)));
+
+        let level: u8 = self.engine.level;
+
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: &format!("  {level}  "),
+            rect: level_text,
+        };
+        text_draw_ctx.draw_text();
+
+        // lines text
+        let lines_text = score_bottom.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
+
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: "SCORE",
+            rect: lines_text,
+        };
+        text_draw_ctx.draw_text();
+
+        // lines text
+        let lines_text = score_bottom.sub_rect((0.8, 0.85), Some((Align::Center, Align::Far)));
+
+        let score = self.engine.score;
+        let mut text_draw_ctx: TextDrawContext = TextDrawContext {
+            canvas: &mut self.canvas,
+            font: &font,
+            text: &format!("  {score}  "),
+            rect: lines_text,
+        };
+        text_draw_ctx.draw_text();
+
+        self.canvas.present();
     }
-
-    let mut up_next_cell_draw_ctx: CellDrawContext<
-        { Engine::SINGLE_TETRIMINO_MATRIX_WIDTH },
-        { Engine::SINGLE_TETRIMINO_MATRIX_HEIGHT },
-    > = CellDrawContext {
-        origin: up_next1.bottom_left(),
-        dims: Vector2::from(up_next1.size()),
-        canvas,
-        matrix: &engine.up_next_matrix,
-    };
-
-    up_next_cell_draw_ctx.draw_matrix();
-
-    let mut remaining_next_cell_draw_ctx: CellDrawContext<
-        { Engine::REMAINING_NEXT_MATRIX_WIDTH },
-        { Engine::REMAINING_NEXT_MATRIX_HEIGHT },
-    > = CellDrawContext {
-        origin: queue1.bottom_left(),
-        dims: Vector2::from(queue1.size()),
-        canvas,
-        matrix: &engine.queue_matrix,
-    };
-
-    remaining_next_cell_draw_ctx.draw_matrix();
-
-    let mut hold_cell_draw_ctx: CellDrawContext<
-        { Engine::SINGLE_TETRIMINO_MATRIX_WIDTH },
-        { Engine::SINGLE_TETRIMINO_MATRIX_HEIGHT },
-    > = CellDrawContext {
-        origin: hold1.bottom_left(),
-        dims: Vector2::from(hold1.size()),
-        canvas,
-        matrix: &engine.hold_matrix,
-    };
-
-    hold_cell_draw_ctx.draw_matrix();
-
-    // up next text
-    let up_next_text = up_next1.sub_rect((0.5, 0.2), Some((Align::Center, Align::Near)));
-
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: "UP NEXT",
-        rect: up_next_text,
-    };
-    text_draw_ctx.draw_text();
-
-    // hold text
-    let hold_text = hold1.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
-
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: "HOLD",
-        rect: hold_text,
-    };
-    text_draw_ctx.draw_text();
-
-    let score_top = score1.sub_rect((1.0, 0.5), Some((Align::Center, Align::Near)));
-    let score_bottom = score1.sub_rect((1.0, 0.5), Some((Align::Center, Align::Far)));
-
-    // level text
-    let level_text = score_top.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
-
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: "LEVEL",
-        rect: level_text,
-    };
-    text_draw_ctx.draw_text();
-
-    // level text
-    let level_text = score_top.sub_rect((0.8, 0.85), Some((Align::Center, Align::Far)));
-
-    let level: u8 = engine.level;
-
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: &format!("  {level}  "),
-        rect: level_text,
-    };
-    text_draw_ctx.draw_text();
-
-    // lines text
-    let lines_text = score_bottom.sub_rect((0.5, 0.25), Some((Align::Center, Align::Near)));
-
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: "SCORE",
-        rect: lines_text,
-    };
-    text_draw_ctx.draw_text();
-
-    // lines text
-    let lines_text = score_bottom.sub_rect((0.8, 0.85), Some((Align::Center, Align::Far)));
-
-    let score = engine.score;
-    let mut text_draw_ctx: TextDrawContext = TextDrawContext {
-        canvas,
-        font,
-        text: &format!("  {score}  "),
-        rect: lines_text,
-    };
-    text_draw_ctx.draw_text();
-
-    canvas.present();
 }
